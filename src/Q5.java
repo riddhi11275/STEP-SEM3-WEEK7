7c2d43180d1fce7369b4802d724075b6131f2605
Riddhi Sharma <riddhisharma89900@gmail.com>
2026-09-19 16:34:42 +0530

    Added Drone

---
+++ src/Q5.java
@@ -0,0 +1,80 @@
+abstract class Drone {
+
+    public abstract String fly();
+}
+
+interface Trackable {
+
+    String getLocation();
+}
+
+class DeliveryDrone extends Drone implements Trackable {
+
+    String id;
+
+    DeliveryDrone(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String fly() {
+        return id + " flying";
+    }
+
+    @Override
+    public String getLocation() {
+        return id + " at Sector 4";
+    }
+}
+
+class ScoutDrone extends Drone {
+
+    String id;
+
+    ScoutDrone(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String fly() {
+        return id + " scouting";
+    }
+}
+
+class GroundRobot implements Trackable {
+
+    String id;
+
+    GroundRobot(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getLocation() {
+        return id + " at Sector 4";
+    }
+}
+
+public class Q5 {
+
+    static String getLocationIfTrackable(Object o) {
+
+        if (o instanceof Trackable) {
+            Trackable t = (Trackable) o;
+            return t.getLocation();
+        } else {
+            return "Tracking not available";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        DeliveryDrone d = new DeliveryDrone("DR-1");
+        ScoutDrone s = new ScoutDrone("SC-1");
+        GroundRobot g = new GroundRobot("GR-1");
+
+        System.out.println(getLocationIfTrackable(d));
+        System.out.println(getLocationIfTrackable(s));
+        System.out.println(getLocationIfTrackable(g));
+    }
+}
